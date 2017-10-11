781c64b509bf104fc48e267e7508b6708533f1fa
rachang <rachang@umich.edu>
2017-10-11 14:09:05 -0400

    Third Commit

--- urllinks.py
+++ urllinks.py
@@ -1,10 +1,3 @@
-# To run this, you can install BeautifulSoup
-# https://pypi.python.org/pypi/beautifulsoup4
-
-# Or download the file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
