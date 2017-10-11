1d6fe78135e86d2988e6ec753ba17394979c649e
rachang <rachang@umich.edu>
2017-10-11 13:46:14 -0400

    Third Commit

--- urllinks.py
+++ urllinks.py
@@ -14,8 +14,10 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-for i in range(7):
+url = input('Enter url- ')
+count = int(input('Enter count- '))
+position = int(input('Enter position- '))
+for i in range(count):
 	html = urllib.request.urlopen(url, context=ctx).read()
 	soup = BeautifulSoup(html, 'html.parser')
 
@@ -25,6 +27,6 @@ for i in range(7):
 	for tag in tags:
 	   	x = tag.get('href', None)
 	   	links.append(x)
-	print(links[17])
-	url = links[17]
-print("Final:", links[17])
+	print(links[position-1])
+	url = links[position-1]
+print("Final:", url)
